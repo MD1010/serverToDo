e9bb478571068bb10bc1a16639a525842b0d4139
unknown <michaelkatom10@gmail.com>
2019-01-22 23:41:27 +0200

    users api done

---
+++ public/utils/validations.js
@@ -0,0 +1,43 @@
+
+import usersCollection from '../../Schemas/userSchema';
+
+function validateFields(req,next){
+    let okFields = true 
+    let { userName, email, password } = req.body
+    const passwordRegex = RegExp(/^.{4,8}$/)
+    const emailRegex = RegExp(
+        /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+        );
+    if(userName.trim().length < 4 || userName.trim().length > 8){
+        okFields = false
+        next({message:"username has to be between 4 and 8 characters"})
+    }
+    if(!passwordRegex.test(password)){
+        okFields = false
+        next({message:"password has to be between 4 and 8 characters"})
+    }
+
+    if(!emailRegex.test(email)){
+        okFields = false
+        next({message:"Invalid email address"})
+    }
+    return okFields
+}
+
+async function checkIfuserExists(req, res, next){
+    let usernameToCheck = req.body.userName
+    let foundUserName = false
+    await usersCollection.findOne({userName:usernameToCheck}).then((result)=>{
+        if(result){
+            next({message:"username already exists"})
+            foundUserName = true 
+        }
+        else{
+            res.status(201).end()
+        }
+           
+    })
+    return foundUserName
+}
+
+export { validateFields, checkIfuserExists}
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import usersCollection from '../Schemas/userSchema';
+import { validateFields, checkIfuserExists } from '../public/utils/validations'
 
 let router = express.Router();
 const app = express();
@@ -8,6 +9,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+//add GET find a userName with specific userName
 router.get('/', (req, res, next)=>
 {
     usersCollection.find({}).then((users)=>
@@ -18,13 +20,15 @@ router.get('/', (req, res, next)=>
 
 router.post('/',(req,res,next)=>
 { 
-    usersCollection.create(req.body).then((newUser)=>
-    {
-        res.json({ userName: newUser.userName, 
-                   email: newUser.email,
-                   password: newUser.password
-                });
-    }).catch(next);
+    if(validateFields(req,next) && !checkIfuserExists(req,res,next)){
+        usersCollection.create(req.body).then((newUser)=>
+        {
+            res.json({ userName: newUser.userName, 
+                       email: newUser.email,
+                       password: newUser.password
+                    });
+        }).catch(next);
+    }
 });
 
 router.delete('/:id',(req,res,next)=>
